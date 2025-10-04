2b53d55da82c06ecb066fbb6e71b1adf55c0c027
cerka@alex <yurasmirnovlynxtram@gmail.com>
2025-10-04 09:53:04 +0900

    create footer component

--- frontend/src/shared/components/Footer/ui/components/FooterNav/ui/FooterNav.tsx
+++ frontend/src/shared/components/Footer/ui/components/FooterNav/ui/FooterNav.tsx
@@ -1,22 +1,24 @@
 import { useState } from 'react';
-import { CatalogItem, type ICatalogItemProp } from '../../../../../Header/ui/components/Catalog/ui/components/CatalogItem';
+import {type ICatalogItemProp } from '../../../../../Header/ui/components/Catalog/ui/components/CatalogItem';
 import css from './index.module.scss';
 import useDeviceType from '../../../../../../hooks/useDeviceType';
+import clsx from 'clsx';
+import { Link } from 'react-router-dom';
 
 const footerNavItem = [
   {
     id: 'footer_link_1',
-    links: [{to: '', text: 'Категории'}, {to: '', text: 'Каталог'},{to: '', text: 'Избранное'},{to: '', text: 'О компании'},{to: '', text: 'Вакансии'}],
+    links: [{to: '#', text: 'Категории'}, {to: '#', text: 'Каталог'},{to: '#', text: 'Избранное'},{to: '#', text: 'О компании'},{to: '#', text: 'Вакансии'}],
     title: 'Название блока'
   },
   {
     id: 'footer_link_2',
-    links: [{to: '', text: 'Категории'}, {to: '', text: 'Каталог'},{to: '', text: 'Избранное'},{to: '', text: 'О компании'},{to: '', text: 'Вакансии'}],
+    links: [{to: '#', text: 'Категории'}, {to: '#', text: 'Каталог'},{to: '#', text: 'Избранное'},{to: '#', text: 'О компании'},{to: '#', text: 'Вакансии'}],
     title: 'Название блока'
   },
   {
     id: 'footer_link_3',
-    links: [{to: '', text: 'Категории'}, {to: '', text: 'Каталог'},{to: '', text: 'Избранное'},{to: '', text: 'О компании'},{to: '', text: 'Вакансии'}],
+    links: [{to: '#', text: 'Категории'}, {to: '#', text: 'Каталог'},{to: '#', text: 'Избранное'},{to: '#', text: 'О компании'},{to: '#', text: 'Вакансии'}],
     title: 'Название блока'
   },
 ];
@@ -35,9 +37,60 @@ export const FooterNav = () => {
         <nav className={css.footer_nav_wrapper}>
             <div className={css.footer_nav_content}>
                 {footerNavItem.map((item) => (
-                  
+                  <FooterNavItem
+                    id={item.id}
+                    isExpanded={deviceType == 'desctop' || deviceType == 'tablet' ? true: expandedId === item.id}
+                    isSelect={selectedId === item.id}   
+                    key={item.id}
+                    title={item.title}
+                    links={item.links}
+                    onSelect={handleSelect}
+                  />
                 ))}
             </div>
         </nav>
     );
+};
+
+
+export const FooterNavItem: React.FC<ICatalogItemProp> = ({ 
+  id, 
+  isSelect, 
+  isExpanded, // Теперь из props
+  links, 
+  title, 
+  onSelect,
+}) => {
+  const deviceType = useDeviceType();
+
+  const handleTitleClick = () => {
+    onSelect?.(id); // Вызываем обработчик из родителя
+
+  };
+
+ 
+
+  return (
+    <div className={clsx(css.footer_item, {[css.open]: isExpanded})}>
+      <ul className={clsx(css.footer_item_wrapper, css[deviceType])} id={id}>
+        <li 
+          className={clsx(css.footer_title, css[deviceType], { [css.select]: isSelect }, {[css.mobile_title_arrow]: isExpanded})} 
+          onClick={handleTitleClick} // Клик на заголовок
+        >
+          {title}
+        </li>
+        <ul className={clsx(css.footer_links_list, { [css.hidden]: !isExpanded }
+
+        )}> {/* Скрываем на основе props */}
+          {(links || []).map((link) => (  
+            <li key={link.to} className={css.footer_link_wrapper}>
+              <Link to={link.to} className={clsx(css.footer_link)}>
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </ul>
+    </div>
+  );
 };
